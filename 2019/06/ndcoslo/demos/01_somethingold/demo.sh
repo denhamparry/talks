c89c08fdb1a0149e6399875cd6ac57ad7c4eaca5
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2019-06-20 09:35:39 +0200

    Demo 1

--- 2019/06/ndcoslo/demos/01_somethingold/demo.sh
+++ 2019/06/ndcoslo/demos/01_somethingold/demo.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-docker stats
-docker run 
+3 terminal windows
+
+$ docker stats
+$ docker run --rm -it -p 8080:80 denhamparry/exploit001:0.0.1
+$ curl localhost:8080
+
+# Visit in browser
+
+$ ./exploit.sh localhost:8080
+
+# What is a CVE?
+
+# The common vulnerabilities and exposures (CVE) program.
+# Cataloging software and firmware vulnerabilities for 18 years.
+
+$ whoami
+$ pwd
+$ ls
+$ ps
